5e07946292bdcf1c5edac3ed41c71e016206d617
guhpissai <gustavopissai3d@gmail.com>
2023-05-26 15:46:48 -0300

    Criando endpoint para listar todos os produtor e pelo id

--- backend/src/controllers/productsController.js
+++ backend/src/controllers/productsController.js
@@ -1,7 +1,17 @@
-const getAll = (req, res) => {
-  res.status(200).json({ message: 'ok!' });
+const productsService = require('../services/productsService');
+
+const getAll = async (req, res) => {
+  const result = await productsService.getAll();
+  res.status(200).json(result);
+};
+
+const getById = async (req, res) => {
+  const { id } = req.params;
+  const result = await productsService.getById(id);
+  res.status(200).json(result);
 };
 
 module.exports = {
   getAll,
+  getById,
 };
--- backend/src/models/productsModel.js
+++ backend/src/models/productsModel.js
@@ -1,12 +1,19 @@
 const connection = require('./connection');
 
 const getAll = async () => {
-  const [result] = connection.execute(
-    'SELECT * FROM products',
+  const [result] = await connection.execute(
+    'SELECT * FROM StoreManager.products',
   );
+  return result;
+};
+
+const getById = async (id) => {
+  const [[result]] = await connection
+    .execute('SELECT * FROM StoreManager.products WHERE id = ? ORDER BY id', [id]);
   return result;
 };
 
 module.exports = {
   getAll,
+  getById,
 };
